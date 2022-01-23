dd256d106fceb1f2850f9efe26725fffe57aa2d2
Likith9676952347 <likithkumarreddy143@gmail.com>
2022-01-23 22:10:59 +0530

    Add project files.

---
+++ ConsoleApp20/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp20
+{
+   public class Employee : Department
+    {
+        public string GetEmployeeFirstName()
+        {
+            return "LUCIFER";
+        }
+
+        public string GetEmployeeLastName()
+        {
+            return "Lkith";
+        }
+
+        public override string GetDepartmentDetails()
+        {
+            return "B-TECH";
+        }
+        public static void Main(string[] args)
+        {
+            Department department = new Department();
+            Department employee = new Employee();
+            Console.WriteLine(department.GetDepartmentDetails()); //calling method of base class
+            Console.WriteLine(employee.GetDepartmentDetails()); //calling method of child class, example of polymorphism 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp20
+{
+    public class Department
+    {
+        public string GetDartmentName()
+        {
+            return "B-TECH";
+        }
+        public int GetNumberOfDepartment()
+        {
+            return 6;
+        }
+        public virtual string GetDepartmentDetails()
+        {
+            return "BTECH_1";
+        }
+    }
+}
